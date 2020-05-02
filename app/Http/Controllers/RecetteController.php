1ed9eece6cfbd4d42178f29d0c7565e5ca512c78
Lotfi <dl_ikhadallem@esi.dz>
2020-05-02 16:32:37 +0100

    delete exit from python file

--- app/Http/Controllers/RecetteController.php
+++ app/Http/Controllers/RecetteController.php
@@ -10,7 +10,7 @@ use GuzzleHttp\Client as GuzzleClient;
 use Config;
 use DomXPath;
 use Barryvdh\Debugbar\Facade as Debugbar;
-
+use Log;
 
 class RecetteController extends Controller
 {
@@ -22,6 +22,7 @@ class RecetteController extends Controller
         ini_set('max_execution_time', 0);
         $result = 0;
         $result = shell_exec('python crawl.py');
+        log::info($result);
         $resultData = json_decode($result, true);
         return $resultData;
     }
