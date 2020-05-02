ef3367d228cbccdad8418304d9b70c457f5cbb00
Lotfi <dl_ikhadallem@esi.dz>
2020-05-02 16:42:19 +0100

    delete comment from python file

--- public/crawl.py
+++ public/crawl.py
@@ -1,13 +1,14 @@
 from random import randrange;
+import time;
+import json ;
 from bs4  import BeautifulSoup;
 import requests ;
-import json ;
 from selenium import webdriver ;
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time;
+
 def verifyAliment(string):
     banned = ["porc","sanglier","vin","chorizo","jambon","cochon"]
     for word in banned:
